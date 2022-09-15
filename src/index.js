f1dc1442154b6e2da9c67c183787781396b28715
Sudipta Mishra <asksudipta@gmail.com>
2022-09-15 15:42:28 +0200

    added bootstrap dependancy and impot  with css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+//import reactDOM to render message.
 import ReactDOM from 'react-dom';
+
+// import the bootsrap resource
+import "bootstrap/dist/css/bootstrap.css";
+
 
 const message = <h1>Hello React App</h1>
 ReactDOM.render(message, document.getElementById('root')  );
